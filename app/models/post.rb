4ea246c023e57559cfe05e8e7aebbbda26fc5612
Harley Rayner <harleymteden@gmail.com>
2019-01-18 16:42:46 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 10 }
   validates :summary , length: { maximum: 100 }
-  # validates :category, presence: true
-
   validates :category, inclusion: { in: %w("Fiction" "Non-Fiction") }
 
 end
