94cf96cc4024828aefb68280565d3aae67471230
Harley Rayner <harleymteden@gmail.com>
2019-01-18 17:01:49 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ end
 
 
 class MyValidator < ActiveModel::Validator
-  def validate(record)
+  def validate(post)
     unless record.name.starts_with? 'X'
       record.errors[:name] << 'Need a name starting with X please!'
     end
