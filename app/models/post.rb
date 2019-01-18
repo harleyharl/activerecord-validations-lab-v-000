d05c093f6b30bca1c389318d6e78f981e3fdb2b7
Harley Rayner <harleymteden@gmail.com>
2019-01-18 17:51:44 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,7 +14,7 @@ class Post < ActiveRecord::Base
   ]
 
   def title_must_be_clickbaity
-    binding.pry
+    # binding.pry
     if CLICKBAIT_PATTERNS.none?  { |pattern| pattern.match(self.title) } #=> true
       errors.add(:title, "False")
     end
