ced57344e92925892e10a3d618b24495f75564dc
Harley Rayner <harleymteden@gmail.com>
2019-01-18 16:44:49 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 10 }
   validates :summary , length: { maximum: 100 }
-  validates :category, inclusion: { in: %w("Fiction" "Non-Fiction") }
+  validates :category, inclusion: { in: %w("Fiction" "Non-Fiction"),
+    message: "%{value} is not a valid size" }
 
 end
